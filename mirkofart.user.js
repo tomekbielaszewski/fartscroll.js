59468c231c2d0f2ebf1cbf6fb7403c2e4921fe07
Grizwold <tomekbielaszewski@gmail.com>
2017-03-27 22:32:13 +0200

    ver increment

--- mirkofart.user.js
+++ mirkofart.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       mirkofart
 // @namespace  http://wykop.pl/
-// @version    1.0
+// @version    1.1
 // @description  #podnosimypoziomwykopu
 // @match      http://www.wykop.pl/*
 // @copyright  2017, @Grizwold, https://github.com/theonion
